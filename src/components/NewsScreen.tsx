a8d46253cc621ffa80c1d72be86726152ec00984
CeNan11 <johnrenanlabay75@gmail.com>
2025-08-04 21:03:22 +0800

    Map some links

--- src/components/NewsScreen.tsx
+++ src/components/NewsScreen.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import type { Article } from "../types/types";
 
 const NewsScreen: React.FC = () => {
-    const [article, setArticle] = useState<Article>();
+    const [articles, setArticle] = useState<Article[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
 
@@ -14,7 +14,7 @@ const NewsScreen: React.FC = () => {
                 const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=457e0eaa43774b17883e3f4641a9456e`);
                 if(!response.ok) throw new Error("Could Not Fetch Data")
                 const data = await response.json();
-                setArticle(data.articles[0]);
+                setArticle(data.articles);
                 setError(null);
             }
             catch(err){
@@ -28,7 +28,21 @@ const NewsScreen: React.FC = () => {
     }, []);
 
   return (
-    <h1>Test case</h1>
+    <div>
+        <div>
+            <h1>Top News Today</h1>
+        </div>
+        {loading && <p>Loading...</p>}
+        {error &&  <p>{error}</p>}
+
+        {articles.map((article: Article, index: number) => (
+        <div key={index}>
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+        </div>
+        ))}
+    </div>
   );
 }
 
